d4ba3048100d3ffb43df4bca403edf0ad1901e05
Iwo Strzeboński <iwo.strzebonski@gmail.com>
2021-06-12 19:43:30 +0200

    Prace nad przesyłem danych do drugiego gracza

--- client/webpack/src/components/Keyboard.js
+++ client/webpack/src/components/Keyboard.js
@@ -14,6 +14,7 @@ const KEYS = {
 
 export default class Keyboard {
     constructor(domElement, animation, mesh, collider, collisions) {
+        this.socket = Config.socket
         this.domElement = domElement
         this.animation = animation
         this.mesh = mesh
@@ -33,6 +34,15 @@ export default class Keyboard {
                     this.mesh.position.x, this.mesh.position.z, 'u'
                 )
 
+                if (collision === 'U') {
+                    Config.moveCrate = this.collisions.searchCrate(
+                        this.mesh.position.x, this.mesh.position.z, 'u'
+                    )
+                    Config.moveCrate.rotation.y = Math.PI / 2
+                } else {
+                    Config.moveCrate = null
+                }
+
                 this.mesh.rotation.y = Math.PI / 2
                 this.collider.rotation.y = Math.PI / 2
             }
@@ -44,6 +54,15 @@ export default class Keyboard {
                 collision = this.collisions.playerWallCollision(
                     this.mesh.position.x, this.mesh.position.z, 'l'
                 )
+
+                if (collision === 'L') {
+                    Config.moveCrate = this.collisions.searchCrate(
+                        this.mesh.position.x, this.mesh.position.z, 'l'
+                    )
+                    Config.moveCrate.rotation.y = Math.PI
+                } else {
+                    Config.moveCrate = null
+                }
 
                 this.mesh.rotation.y = Math.PI
                 this.collider.rotation.y = Math.PI
@@ -57,6 +76,15 @@ export default class Keyboard {
                     this.mesh.position.x, this.mesh.position.z, 'd'
                 )
 
+                if (collision === 'D') {
+                    Config.moveCrate = this.collisions.searchCrate(
+                        this.mesh.position.x, this.mesh.position.z, 'd'
+                    )
+                    Config.moveCrate.rotation.y = -Math.PI / 2
+                } else {
+                    Config.moveCrate = null
+                }
+
                 this.mesh.rotation.y = -Math.PI / 2
                 this.collider.rotation.y = -Math.PI / 2
             }
@@ -68,6 +96,15 @@ export default class Keyboard {
                 collision = this.collisions.playerWallCollision(
                     this.mesh.position.x, this.mesh.position.z, 'r'
                 )
+                
+                if (collision === 'R') {
+                    Config.moveCrate = this.collisions.searchCrate(
+                        this.mesh.position.x, this.mesh.position.z, 'r'
+                    )
+                    Config.moveCrate.rotation.y = 0
+                } else {
+                    Config.moveCrate = null
+                }
 
                 this.mesh.rotation.y = 0
                 this.collider.rotation.y = 0
@@ -81,6 +118,8 @@ export default class Keyboard {
         Config.move = collision !== false
 
         if (Config.move && collision !== undefined) {
+            console.log(collision)
+            this.socket.emit('move', collision)
             this.animation.playAnim('CrWalk')
             document.getElementById('moves').innerText++
         }
